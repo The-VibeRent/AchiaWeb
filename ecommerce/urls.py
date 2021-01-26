622fc82abf518a942f475ebec87aec9baaa5964a
Jay Prakash <unijay12@gmail.com>
2021-01-26 16:19:23 +0530

    1st commit

---
+++ ecommerce/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('core.urls', namespace='core')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
