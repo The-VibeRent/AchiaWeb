1e510813d654b6365adbdd17e539b4182f0b166c
Jay Prakash <unijay12@gmail.com>
2021-02-02 11:28:56 +0530

    urls changed and product url changed accordingly

--- core/urls.py
+++ core/urls.py
@@ -26,10 +26,10 @@ urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<id>/', ItemDetailView, name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add-to-cart/<id>/', add_to_cart, name='add-to-cart'),
     path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+    path('remove-from-cart/<id>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<id>/', remove_single_item_from_cart,
          name='remove-single-item-from-cart'),
     path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
     path('request-refund/', RequestRefundView.as_view(), name='request-refund')
